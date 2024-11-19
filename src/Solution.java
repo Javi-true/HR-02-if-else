7ac3566c7cdb1498f0b79ba9ebf2b9594ae63028
Javier Trujillo <rivera.dev@outlook.com>
2024-11-18 19:28:34 -0600

    Finished

--- src/Solution.java
+++ src/Solution.java
@@ -15,5 +15,16 @@ public class Solution {
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
         scanner.close();
+
+        if(N % 2 != 0){
+            System.out.println("Weird");
+        }else if(N <= 5 && N >= 2){
+            System.out.println("Not Weird");
+        }else if(N <= 20 && N >= 6){
+            System.out.println("Weird");
+        }else if(N>20){
+            System.out.println("Not Weird");
+        }
+
     }
 }
